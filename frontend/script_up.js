f5a94f0202a4201d9d5527556cd6c41cc19c67cd
aditya-chari25 <adi25charis@gmail.com>
2022-05-27 20:38:20 +0530

    "button function"

--- frontend/script_up.js
+++ frontend/script_up.js
@@ -20,3 +20,34 @@ function myFunction(){
         document.getElementById("context-q").innerHTML=text
     })
 }
+
+function sentload(){
+    window.count = window.count+1
+    document.getElementById("prevb").disabled=false
+    window.file_name = localStorage.getItem("file_ml");  
+    window.file_id = localStorage.getItem("idq");
+    fetch(`http://localhost:5000/${window.file_name}/${window.file_id}/${window.count}`)
+    .then((res)=> res.json())
+    .then((data)=> {
+        data = JSON.parse(data)
+        text=""
+        text = text + `<p>${data["contents"][window.count]}</p>`;
+        document.getElementById("context-q").innerHTML=text
+    })
+
+}
+
+function prevload(){
+    window.count = window.count-1
+    window.file_name = localStorage.getItem("file_ml");  
+    window.file_id = localStorage.getItem("idq");
+    fetch(`http://localhost:5000/${window.file_name}/${window.file_id}/${window.count}`)
+    .then((res)=> res.json())
+    .then((data)=> {
+        data = JSON.parse(data)
+        text=""
+        text = text + `<p>${data["contents"][window.count]}</p>`;
+        document.getElementById("context-q").innerHTML=text
+    })
+
+}
